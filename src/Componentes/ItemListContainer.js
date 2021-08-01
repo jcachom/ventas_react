2fecd283ce4b009f7f3949164a7c0e3843195f27
jcachom <jcachom10@gmail.com>
2021-08-01 08:23:51 -0500

    Desafío 04 - v2

--- src/Componentes/ItemListContainer.js
+++ src/Componentes/ItemListContainer.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
  
 class  ItemListContainer extends  Component {
     render() {
-    
+        const colorP={background:'yellow'}
+
         return (
-            <div>
+            <div style={colorP}>
             {this.props.titulo}
             </div>
              );
