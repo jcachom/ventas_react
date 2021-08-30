25a4c57850a62e44d737184a906d536dd806e3ce
jcachom <jcachom10@gmail.com>
2021-08-29 21:31:54 -0500

    Revisión Eventos

--- src/Componentes/ItemDetail.js
+++ src/Componentes/ItemDetail.js
@@ -3,6 +3,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css' ;
 import ItemCount from './ItemCount';
 import { useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
  
 
   const  ItemDetail = (props) =>{
@@ -10,28 +11,53 @@ import { useHistory } from 'react-router-dom';
 
     const history =useHistory();    
     const cart = `${process.env.PUBLIC_URL}/Carrito`
-    const onVerCarrito =() => history.push(cart);
+    const onVerCarrito =() =>
+    {
+      history.push(cart);
+    } 
+
+    const onAddCarrito =() =>
+    {
+      setVisible(false);
+    } 
+
+    const [visible, setVisible] = useState(true);
 
 
     
   
         const colorP={'font-weight': 'bold'}
+        const colorAdic={'color': 'green'}
+
         const borde = {
           'border': '3px solid green'
           };
 
+       
+          let styleDefault = { display: "inline" };
+          let styleCart = { display: "none" };
+          if (!visible) styleDefault.display = "none";
+          if (!visible) styleCart.display = "inline";
+
+                 
             return (
     
              <div style={borde}>
-            <div><span style={colorP} >Detalle Producto</span> </div>
-           <img src={require(`../img/${props.nombre_img}`).default}></img>
+             <div><span style={colorP} >Detalle Producto</span> </div>
+             <img src={require(`../img/${props.nombre_img}`).default}></img>
              <div >  <span style={colorP} >Producto:</span>  {props.producto}  <span style={colorP}>Precio:</span> {props.precio}</div>
              <div >  <span style={colorP} >Detalle:</span>  {props.tipo} </div>
              <div >  <span style={colorP} >En Stock:</span>  {props.stock} </div>
              
              <ItemCount cant_inicial="1" stock= {props.stock}></ItemCount>
 
-             <div>  <button className="btn btn-primary" onClick={onVerCarrito}>Terminar mi compra</button></div>
+          
+             <div style={styleCart} ><span style={colorAdic} >Producto adicionado</span> </div>
+
+             <div  style={styleDefault}>  <button className="btn btn-primary" onClick={onAddCarrito}>Agregar carrito</button></div>
+
+             <div  style={styleCart}>  <button className="btn btn-warning" onClick={onVerCarrito}>Ir al carrito</button></div>
+
 
             </div>
    
